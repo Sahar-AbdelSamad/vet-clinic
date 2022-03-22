65a77076065e325ac2a0630c9051147e8aa450e3
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-03-22 11:42:10 +0200

    Add queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,27 @@
 /*Queries that provide answers to the questions from all projects.*/
-
-SELECT * from animals WHERE name = 'Luna';
+Select *
+from animals
+where name like '%mon';
+Select *
+from animals
+WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+Select *
+from animals
+WHERE escape_attempts < 3
+    AND neutered = true;
+Select *
+from animals
+WHERE name in ('Agumon', 'Pikachu');
+Select name,
+    escape_attempts
+from animals
+WHERE weight_kg > 10.5;
+Select *
+from animals
+WHERE neutered = true;
+Select *
+from animals
+WHERE name != 'Gabumon';
+Select *
+from animals
+WHERE weight_kg BETWEEN 10.4 AND 17.3;
