f0bff79ac01d9280a19cdf923eed3399d7c809ab
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-03-23 22:17:17 +0200

    Create owners and species tables

--- schema.sql
+++ schema.sql
@@ -1,12 +1,35 @@
 /* Database schema to keep the structure of entire database. */
 CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id int GENERATED ALWAYS AS IDENTITY,
     name varchar(100),
-    date_of_birth DATE,
-    escape_attempts INT,
-    neutered BOOLEAN,
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
     weight_kg DECIMAL,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
+
 ALTER TABLE animals
-add species varchar(100);
+    ADD species varchar(100);
+
+CREATE TABLE owners (
+    id int GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(100),
+    age int,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id int GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE animals
+    DROP species;
+
+ALTER TABLE animals
+    ADD species_id INT REFERENCES species (id);
+
+ALTER TABLE animals
+    ADD owner_id INT REFERENCES owners (id);
