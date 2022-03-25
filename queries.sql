d0cec958be452c5234b3ec64899ede9b0bfee285
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-03-25 11:24:25 +0200

    Add SQL queries to select data from tables

--- queries.sql
+++ queries.sql
@@ -171,7 +171,6 @@ GROUP BY
     species;
 
 -- Milestone 3
-
 SELECT
     owners.full_name AS owner,
     animals.name
@@ -239,5 +238,130 @@ GROUP BY
     owners.full_name
 ORDER BY
     count(animals.*) DESC
+LIMIT 1;
+
+--milestone 4
+SELECT
+    animals.name AS animal,
+    visits.date_of_visit
+FROM
+    visits
+    JOIN animals ON animals.id = visits.animals_id
+    JOIN vets ON vets.id = visits.vets_id
+WHERE
+    vets.name = 'William Tatcher'
+ORDER BY
+    date_of_visit DESC
+LIMIT 1;
+
+SELECT
+    count(new.*) AS "Different animals seen by Stephanie Mendez"
+FROM (
+    SELECT
+        animals.name
+    FROM
+        visits
+        JOIN vets ON visits.vets_id = vets.id
+        JOIN animals ON animals.id = visits.animals_id
+    WHERE
+        vets.name = 'Stephanie Mendez'
+    GROUP BY
+        animals.name) AS new;
+
+SELECT
+    vets.name AS vet,
+    species.name AS specialties
+FROM
+    specializations AS s
+    RIGHT JOIN vets ON vets.id = s.vets_id
+    LEFT JOIN species ON species.id = s.species_id;
+
+SELECT
+    vets.name AS vet,
+    animals.name AS animals,
+    visits.date_of_visit
+FROM
+    visits
+    JOIN vets ON vets.id = visits.vets_id
+    JOIN animals ON animals.id = visits.animals_id
+WHERE
+    visits.date_of_visit BETWEEN '2020-04-01' AND '2020-08-30'
+    AND vets.name = 'Stephanie Mendez';
+
+SELECT
+    animals.name AS Animal,
+    count(visits.date_of_visit) AS "Number of visits"
+FROM
+    visits
+    JOIN animals ON animals.id = visits.animals_id
+GROUP BY
+    animals.name
+ORDER BY
+    count(visits.date_of_visit) DESC
+LIMIT 1;
+
+SELECT
+    vets.name AS vets,
+    animals.name AS animal,
+    visits.date_of_visit
+FROM
+    visits
+    JOIN animals ON animals.id = visits.animals_id
+    JOIN vets ON vets.id = visits.vets_id
+WHERE
+    vets.name = 'Maisy Smith'
+ORDER BY
+    visits.date_of_visit
+LIMIT 1;
+
+SELECT
+    animals.name AS animal,
+    animals.date_of_birth,
+    animals.neutered,
+    animals.escape_attempts,
+    animals.weight_kg,
+    vets.name AS vet,
+    vets.age,
+    vets.date_of_graduation,
+    visits.date_of_visit
+FROM
+    animals
+    JOIN visits ON animals.id = visits.animals_id
+    JOIN vets ON vets.id = visits.vets_id
+ORDER BY
+    visits.date_of_visit DESC
+LIMIT 1;
+
+SELECT
+    count(new.*) AS "Visits to vet not specialized in that animal's species"
+FROM (
+    SELECT
+        vets.name AS vet,
+        s.species_id AS specialization,
+        animals.species_id
+    FROM
+        specializations AS s
+        JOIN vets ON vets.id = s.vets_id
+        JOIN visits ON visits.vets_id = vets.id
+        JOIN animals ON animals.id = visits.animals_id
+    WHERE
+        vets.name <> 'Stephanie Mendez') AS new
+WHERE
+    new.specialization <> new.species_id;
+
+SELECT
+    count(species.*),
+    species.name AS specie
+FROM
+    visits
+    JOIN vets ON visits.vets_id = vets.id
+    JOIN animals ON animals.id = visits.animals_id
+    JOIN species ON species.id = animals.species_id
+WHERE
+    vets.name = 'Maisy Smith'
+GROUP BY
+    species.name
+ORDER BY
+    count DESC
 LIMIT 1;
 
